57a3c00a6e1259d226200a173d7b02e43c016244
otto <types2d@bk.ru>
2023-08-28 01:18:16 +0600

    portfolio1
    test

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+jQuery(window).on('load', function() {
+	"use strict";
+    
+    
+   
+    $(".preloader").addClass("hide-preloader");   
+    
+    
+    setTimeout(function(){
+
+        $("#intro .animation-container").each(function() {
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+    
+});
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+    
+    
+    
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
+        
+    });
+    
+    
+    
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated", { 
+        duration: 600,
+        delay: 0,
+        origin: "left",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+    
+    
+   
+    
+});
